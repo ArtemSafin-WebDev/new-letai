9ff7f7045d773a9f2a2f28ce2fb4d6683fb15b7f
gilyazov <gilyazov.aydar@gmail.com>
2024-12-16 15:02:14 +0300

    фиксы

--- src/js/menu.ts
+++ src/js/menu.ts
@@ -7,4 +7,13 @@ export default function menu() {
     event.preventDefault();
     document.body.classList.toggle("menu-open");
   });
+
+  const links = Array.from(
+      document.querySelectorAll<HTMLAnchorElement>(".page-header__nav ul li a")
+  );
+  links.forEach((link) => {
+    link?.addEventListener("click", () => {
+      document.body.classList.remove("menu-open");
+    });
+  });
 }
