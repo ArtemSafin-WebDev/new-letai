81001ab5e93b2932c224a8565d1085549f055e2e
gilyazov <gilyazov.aydar@gmail.com>
2025-07-10 23:09:45 +0300

    gpon

--- src/js/forms.ts
+++ src/js/forms.ts
@@ -9,10 +9,52 @@ export default function forms() {
     const validator = new Validator(form);
     form.validator = validator;
     form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
       validator.validate();
       if (!validator.valid) {
-        event.preventDefault();
+        return;
       }
+
+      // @ts-ignore
+      const wait = BX.showWait(form);
+
+
+      const data = {
+        // @ts-ignore
+        firstname: form.elements['firstname'].value,
+        // @ts-ignore
+        phone: form.elements['phone'].value
+      };
+
+      // @ts-ignore
+      BX.ajax.runComponentAction('tattelecom:form',
+          'sendLead', {
+            mode: 'class',
+            // @ts-ignore
+            data: {post: data}, // ключи объекта data соответствуют параметрам метода
+          })
+          // @ts-ignore
+          .then(function(response) {
+            if (response.status === 'success') {
+
+              if (response.data.status === false){
+                alert(response.data.error_text);
+              }
+              else{
+                form.reset();
+
+                alert("Заявка отправлена!")
+              }
+            }
+          },
+          // @ts-ignore
+          function (response){
+            alert("Ошибка")
+          });
+
+      // @ts-ignore
+      BX.closeWait(form, wait);
     });
   });
 }
